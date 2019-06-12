e06ac163d3b80f4ff225175fdebbdbfd19b36643
yj <yj@git.com>
2019-06-12 23:11:12 +0800

    删除 /virtqemu文件夹 glibc2.18

--- wps/glibc-2.18/build/csu/version-info.h
+++
@@ -1,6 +0,0 @@
-"Compiled on a Linux 3.10.0 system on 2019-06-09.\n"
-"Available extensions:\n"
-"	crypt add-on version 2.1 by Michael Glad and others\n"
-"	GNU Libidn by Simon Josefsson\n"
-"	Native POSIX Threads Library by Ulrich Drepper et al\n"
-"	BIND-8.2.3-T5B\n"
